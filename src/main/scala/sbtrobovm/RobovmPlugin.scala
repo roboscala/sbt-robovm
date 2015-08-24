8591476a459ba20c862264f154400d32fed9b859
Darkyenus <darkyenus@gmail.com>
2015-08-24 13:38:56 +0200

    Change from plugin to autoplugin
    Removes the iOSProject and NativeProject in favor of cleaner explicitly-added settings.
    Autoplugin is actually needed just for adding the tool-settings (robovmLicense) to every project.
    
    Renames a few keys to prevent potential collisions and to be more consistent.

--- src/main/scala/sbtrobovm/RobovmPlugin.scala
+++ src/main/scala/sbtrobovm/RobovmPlugin.scala
@@ -5,8 +5,8 @@ import sbt._
 
 import scala.xml.Elem
 
-object RobovmPlugin extends Plugin with RobovmUtils {
-  val RoboVMVersion = BuildInfo.version
+object RobovmPlugin extends AutoPlugin with RobovmUtils {
+  val RoboVMVersion:String = BuildInfo.version
 
   /* General settings and tasks */
   val robovmHome = taskKey[Config.Home]("Return the home of RoboVM installation. Will download to local maven repository by default.")
@@ -27,10 +27,10 @@ object RobovmPlugin extends Plugin with RobovmUtils {
   val ipadSim = taskKey[Unit]("Start package on ipad simulator")
   val ipa = taskKey[Unit]("Create an ipa file for the app store")
 
-  val skipSigning = settingKey[Option[Boolean]]("Whether to override signing behavior")
-  val provisioningProfile = settingKey[Option[String]]("Specify provisioning profile to use when signing iOS code.")
-  val signingIdentity = settingKey[Option[String]]("Specify signing identity to use when signing iOS code.")
-  val preferredDevices = settingKey[Seq[String]]("List of iOS device ID's from which device will be chosen if multiple are detected.")
+  val robovmSkipSigning = settingKey[Option[Boolean]]("Whether to override signing behavior")
+  val robovmProvisioningProfile = settingKey[Option[String]]("Specify provisioning profile to use when signing iOS code.")
+  val robovmSigningIdentity = settingKey[Option[String]]("Specify signing identity to use when signing iOS code.")
+  val robovmPreferredDevices = settingKey[Seq[String]]("List of iOS device ID's from which device will be chosen if multiple are detected.")
   // Native Only
   val native = taskKey[Unit]("Run as native console application")
   val nativeBuild = taskKey[Unit]("Compile and archive for distribution as native application")
@@ -40,6 +40,48 @@ object RobovmPlugin extends Plugin with RobovmUtils {
   // iOS Only
   val simulatorDevices = taskKey[Unit]("Prints all available simulator devices to be used in simulator-device-name setting")
 
-  val iOSProject = RobovmProjects.iOSProject
-  val NativeProject = RobovmProjects.NativeProject
+  object autoImport {
+    val RoboVMVersion = RobovmPlugin.RoboVMVersion
+
+    val robovmHome = RobovmPlugin.robovmHome
+    val robovmInputJars = RobovmPlugin.robovmInputJars
+    val robovmVerbose = RobovmPlugin.robovmVerbose
+    val robovmSimulatorDevice = RobovmPlugin.robovmSimulatorDevice
+    val robovmProperties = RobovmPlugin.robovmProperties
+    val robovmConfiguration = RobovmPlugin.robovmConfiguration
+    val robovmDebugPort = RobovmPlugin.robovmDebugPort
+    val robovmDebug = RobovmPlugin.robovmDebug
+    val robovmTarget64bit = RobovmPlugin.robovmTarget64bit
+
+    /* Specific settings and tasks */
+    // iOS Only
+    val simulator = RobovmPlugin.simulator
+    val device = RobovmPlugin.device
+    val iphoneSim = RobovmPlugin.iphoneSim
+    val ipadSim = RobovmPlugin.ipadSim
+    val ipa = RobovmPlugin.ipa
+
+    val robovmSkipSigning = RobovmPlugin.robovmSkipSigning
+    val robovmProvisioningProfile = RobovmPlugin.robovmProvisioningProfile
+    val robovmSigningIdentity = RobovmPlugin.robovmSigningIdentity
+    val robovmPreferredDevices = RobovmPlugin.robovmPreferredDevices
+    // Native Only
+    val native = RobovmPlugin.native
+    val nativeBuild = RobovmPlugin.nativeBuild
+
+    /* Tools */
+    val robovmLicense = RobovmPlugin.robovmLicense
+    // iOS Only
+    val simulatorDevices = RobovmPlugin.simulatorDevices
+
+    //Settings
+    val iOSRoboVMSettings = RobovmProjects.baseSettings ++ RobovmProjects.iOSProjectSettings
+    val nativeRoboVMSettings = RobovmProjects.baseSettings ++ RobovmProjects.nativeProjectSettings
+  }
+
+  //This makes sure that settings below are applied
+  override def trigger: PluginTrigger = allRequirements
+
+  //This adds project-independent tools into the settings
+  override def globalSettings: Seq[Def.Setting[_]] = RobovmProjects.toolSettings
 }
